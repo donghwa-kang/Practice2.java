61e46f34c0e68b25dd7987e2d404093e8a757320
Donghwa Kang <donghwa0730@kw.ac.kr>
2024-09-12 17:03:01 +0900

    test

---
+++ src/Practice2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Practice2{
+    public static void main(String[] args) {
+        // Scanner 객체를 사용하여 입력 받기
+        Scanner scanner = new Scanner(System.in);
+
+        // 사용자에게 메시지 출력
+        System.out.print("이름을 입력하세요: ");
+        String name = scanner.nextLine();
+
+        // 입력된 이름 출력
+        System.out.println("안녕하세요, " + name + "님!");
+
+        // Scanner 객체 닫기
+        scanner.close();
+    }
+}
